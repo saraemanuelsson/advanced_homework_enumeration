80a381c8b89e3565ed05b47504c1730dcf7eeb64
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-06-17 21:17:52 +0100

    Add find anagrams function

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,9 +1,16 @@
 const AnagramFinder = function (word) {
-
+    this.word = word.split("");
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
 
-}
+    return otherWords.filter((otherWord) => {
+        return otherWord.split("").every((letter) => {
+            return this.word.includes(letter);
+        });
+    });
+
+};
 
 module.exports = AnagramFinder;
+
