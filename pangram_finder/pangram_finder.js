0286624823c0d39eee7f12cb1312028f313c8fd9
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-06-17 19:26:42 +0100

    Add method to turn phrase into array of only letters and a method to detect if phrase includes all letters of the alphabet

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,23 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
-}
+  this.phrase = phrase;
+};
+
+PangramFinder.prototype.phraseToLettersArray = function () {
+  const phraseToArray = this.phrase.toLowerCase().split("");
+  return phraseToArray.filter((character) => {
+    if (this.alphabet.includes(character)) {
+      return character
+    };
+  });
+};
 
 PangramFinder.prototype.isPangram = function () {
 
-}
+    return this.alphabet.every((letter) => {
+      return this.phraseToLettersArray().includes(letter);
+    });
+    
+  };
 
 module.exports = PangramFinder;
